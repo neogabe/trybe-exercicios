77cb9e02ea6c2e5d30d4b986e74597aa3516b7a1
Gabriel Lima <glima7591@gmail.com>
2022-03-07 15:57:42 -0300

    exercicios de operadores aritmeticos

---
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Primeiros Passos/app.js
@@ -0,0 +1,24 @@
+// Adição
+const a = 2;
+const b = 6;
+console.log(a + b);
+
+// Substração
+const c = 2;
+const d = 6;
+console.log(c - d);
+
+// Multiplicação
+const e = 2;
+const f = 6;
+console.log(e * f);
+
+// Divisão
+const g = 2;
+const h = 6;
+console.log(g / h);
+
+// Módulo (resto da divisão)
+const i = 2;
+const j = 6;
+console.log(i % j);
