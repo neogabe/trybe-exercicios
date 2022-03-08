1b8a306c4b9fedf0c1afaa89204bb7e025433fbc
Gabriel Lima <glima7591@gmail.com>
2022-03-08 16:08:39 -0300

    exercicio 3

--- Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Array e For loop/app.js
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Array e For loop/app.js
@@ -1,8 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let media = 0;
+
+// Média aritmética é o valor total dos números dentro do array (278) divido pela quantidade de números dentro do array (10)
 
 for (let i = 0; i < numbers.length; i++) {
-  soma += numbers[i]
+  media += numbers[i] / numbers.length
+
 }
 
-console.log(soma);
+console.log(media);
