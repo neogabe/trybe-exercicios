4a9f0f910c424e348b74788a9e513513cb71b3ee
Gabriel Lima <glima7591@gmail.com>
2022-03-08 15:47:05 -0300

    exercicio 1

--- Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Array e For loop/app.js
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Array e For loop/app.js
@@ -1,5 +1,5 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (name of names) {
-  console.log(name);
+for(let i = 0; i < numbers.length; i+= 1) {
+  console.log(numbers[i]);
 }
