6e7f3a790552c09d5641845c8bbb17ee9d057019
Gabriel Lima <glima7591@gmail.com>
2022-03-08 13:56:29 -0300

    adicionando exercicios de ontem

--- Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Primeiros Passos/app.js
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Primeiros Passos/app.js
@@ -1,24 +1,6 @@
-// Adição
-const a = 2;
-const b = 6;
-console.log(a + b);
+const salario = 1500;
 
-// Substração
-const c = 2;
-const d = 6;
-console.log(c - d);
-
-// Multiplicação
-const e = 2;
-const f = 6;
-console.log(e * f);
-
-// Divisão
-const g = 2;
-const h = 6;
-console.log(g / h);
-
-// Módulo (resto da divisão)
-const i = 2;
-const j = 6;
-console.log(i % j);
+//Desconto do INSS
+if(salario <= 1556.94) {
+  salario -
+}
