414692ff2c3213712dbc3f433bf8a68b5d99e19b
Gabriel Lima <glima7591@gmail.com>
2022-03-10 17:27:08 -0300

    exercicio 6 - parte 1

--- Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Objetos e Funções/app.js
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Objetos e Funções/app.js
@@ -1,24 +1,14 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
 
-info.recorrente = 'Sim';
-
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim'
-}
-
-function isRecorrente() {
- if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
-   return 'Ambos recorrentes'
- } else {
-   return ''
- }
-}
-
-console.log(`${info.personagem} e ${info2.personagem}\n${info.origem} e ${info2.origem}\n${info.nota} e ${info2.nota}\n${isRecorrente()}`);
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`);
