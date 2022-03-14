fcb58f9a21e3d1c19043589f47a33df505ed842a
Gabriel Lima <glima7591@gmail.com>
2022-03-14 15:18:10 -0300

    adicionando nova branch e novos arquivos

--- Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Objetos e Funções/app.js
+++ Fundamentos/Introdução a Javascript e Lógica de Programação/Javascript - Objetos e Funções/app.js
@@ -1,15 +1,6 @@
-function isPalindrome(str) {
-  let backward = str.length - 1
-  for (let i = 0; i < str.length; i++) {
-    let x = str[i]
-    let y = str[backward - i]
-    if (x != y) {
-      return false
-    } else {
-      return true
-    }
-  }
-}
+let a = "Serei a melhor pessoa dev";
+let b = "Esse é meu jeito dev de ser!";
+let num = 2;
+let strings = [a, b];
 
-console.log(isPalindrome('arara')); // true
-console.log(isPalindrome('loving')); // false
+console.log((typeof strings.length) == (typeof num));
